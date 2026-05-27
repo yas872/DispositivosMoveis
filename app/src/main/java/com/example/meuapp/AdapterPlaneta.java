3e41abf33c8d34df5094a83b8c952c6355273166
yas872 <alinefelimafra@gmail.com>
2026-05-27 18:58:01 -0300

    plan

---
+++ app/src/main/java/com/example/meuapp/AdapterPlaneta.java
@@ -0,0 +1,40 @@
+package com.example.meuapp;
+import android.content.Context;
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import  android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+public class AdapterPlaneta extends ArrayAdapter<Planeta> {
+    Context lcontext;
+    public AdapterPlaneta(@NonNull Context context, int resource, @NonNull List<Planeta> objects) {
+        super(context, resource, objects);
+        lcontext=context;
+    }
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        //Inflar layout xml
+        LayoutInflater inflater = LayoutInflater.from(lcontext);
+        View itemView=inflater.inflate(R.layout.item_lista,parent,false);
+
+        //preencher item da listagem na variavel view
+        TextView tvNome = itemView.findViewById(R.id.textView);
+        ImageView imageView= itemView.findViewById(R.id.imageView);
+        Planeta p = getItem(position);
+        TextView tvmassa=itemView.findViewById(R.id.tvmassa);
+
+        imageView.setImageResource(p.foto);
+        tvmassa.setText(p.massa);
+
+        tvNome.setText(p.nome);
+        return itemView;
+    }
+}
