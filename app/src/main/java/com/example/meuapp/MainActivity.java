00ed004179d482ea66acf822c88a9f70e7014ec7
yas872 <alinefelimafra@gmail.com>
2026-05-27 18:18:06 -0300

    cont

--- app/src/main/java/com/example/meuapp/MainActivity.java
+++ app/src/main/java/com/example/meuapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.meuapp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,15 +10,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.io.File;
-
 public class MainActivity extends AppCompatActivity {
+
+    Button b;
+    int contador=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        b = findViewById(R.id.button);
+        TextView tv = findViewById(R.id.tv);
+
+
+        b.setOnClickListener(v -> {
+            contador++;
+            tv.setText(Integer.toString(contador));
+
+        });
 
     }
 }
