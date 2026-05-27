a762a760cab9e8be58f7f9b2cfa17cc09c35555b
yas872 <alinefelimafra@gmail.com>
2026-05-27 18:57:19 -0300

    plan

--- app/src/main/java/com/example/meuapp/MainActivity.java
+++ app/src/main/java/com/example/meuapp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.meuapp;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,15 +12,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.io.File;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView lv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        lv=findViewById(R.id.listview);
+        //recuperar dados datasource
+        PlanetaController pcontroller = new PlanetaController();
+
+        AdapterPlaneta adaptador = new AdapterPlaneta(getApplicationContext(),R.layout.item_lista,pcontroller.listaPlanetas());
+        lv.setAdapter(adaptador);
+
 
     }
 }
