c78c74afacb89b8bdd23f778229a33a4158dac0c
yas872 <alinefelimafra@gmail.com>
2026-05-27 19:40:48 -0300

    tela2

--- app/src/main/java/com/example/meuapp/MainActivity.java
+++ app/src/main/java/com/example/meuapp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.meuapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,15 +12,26 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.io.File;
-
 public class MainActivity extends AppCompatActivity {
 
+    Button btn;
+    EditText et;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.button);
+        et = findViewById(R.id.editText);
+
+        btn.setOnClickListener(v -> {
+            Intent i = new Intent(getApplicationContext(), Atividade2.class);
+            String msg=et.getText().toString();
+            i.putExtra("msg", msg);
+            startActivity(i);
+        });
+
 
     }
 }
