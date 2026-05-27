873da3e4e57de2f545644b19a77b0bae7bd15368
yas872 <alinefelimafra@gmail.com>
2026-05-27 18:58:50 -0300

    plan

---
+++ app/src/main/java/com/example/meuapp/PlanetaController.java
@@ -0,0 +1,12 @@
+package com.example.meuapp;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+public class PlanetaController {
+    PlanetaDAO planetaDAO;
+    public PlanetaController() {
+        planetaDAO=new PlanetaDAO();
+    }
+    public ArrayList<Planeta> listaPlanetas(){
+        return planetaDAO.listaPlanetas();
+    }
+}
