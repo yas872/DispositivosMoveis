eb05988b2f318b499d328162f385e1d6e7502bd0
yas872 <alinefelimafra@gmail.com>
2026-05-27 18:33:08 -0300

    list
    Added a ListView to display names and handle item clicks.

--- app/src/main/java/com/example/meuapp/MainActivity.java
+++ app/src/main/java/com/example/meuapp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.meuapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,15 +12,32 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.io.File;
-
 public class MainActivity extends AppCompatActivity {
+
+
+    String nomes[] = new String[]{"Yasmin","Gustavo","Chloe","Ale","Aline","Agustico","Marly"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        getString(R.string.app_name);
 
+        ListView listView = findViewById(R.id.listView);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
+                this, //contexto
+                android.R.layout.simple_list_item_1, //Recurso de layout xml
+                android.R.id.text1, //id do textView interno do layout
+                nomes // Array de String DataSource
+        );
+
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+            Intent i = new Intent(getApplicationContext(), Clicado.class);
+            i.putExtra("nome",nomes[position]);//["nome", valor
+            startActivity(i);
+        });
     }
 }
