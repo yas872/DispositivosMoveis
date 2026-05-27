f4614f266e9c5e6218c7c96762a1b5fe9ddfcace
yas872 <alinefelimafra@gmail.com>
2026-05-27 18:34:15 -0300

    list

---
+++ app/src/main/java/com/example/meuapp/Clicado.java
@@ -0,0 +1,30 @@
+package com.example.meuapp;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Clicado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_clicado);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        String nome = getIntent().getExtras().getString("nome");
+        TextView tv = findViewById(R.id.tvClicado);
+        tv.setText(nome);
+
+    }
+}
