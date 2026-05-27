51783c0d5f8bfdaf77c82cde248d2af628da9832
yas872 <alinefelimafra@gmail.com>
2026-05-27 18:58:25 -0300

    plan

---
+++ app/src/main/java/com/example/meuapp/Planeta.java
@@ -0,0 +1,15 @@
+package com.example.meuapp;
+
+import android.graphics.Bitmap;
+
+public class Planeta {
+    String nome;
+    int foto;
+    String massa;
+
+    public Planeta(String nome, int foto, String massa) {
+        this.nome = nome;
+        this.foto = foto;
+        this.massa = massa;
+    }
+}
